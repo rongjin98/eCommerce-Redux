5dd60654526cac379c6097344184449253066cf4
rongjin98 <91577205+rongjin98@users.noreply.github.com>
2022-07-02 01:47:53 -0400

    'Added-Products'

--- pro_shop/src/App.js
+++ pro_shop/src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Container } from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
+    </>
   );
 }
 
--- readMe.md
+++ readMe.md
@@ -6,3 +6,13 @@
 4. Git
 5. Postman - backend-test
 6. MongoDB
+7. Bootstrap
+8. React-bootstrap
+
+
+
+## 2. Useful Source
+
+* [font awesome cdn](https://cdnjs.com/libraries/font-awesome)
+  * [Icons](https://fontawesome.com/icons)
+* [React-bootstrap](https://react-bootstrap.github.io/components/alerts)
